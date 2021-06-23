5cd545ccabdf23d9a578cda53cf6f0b24f9795dc
Santhosh Sonti <ssonti@spektre.ai>
2021-06-23 15:32:23 -0700

    fix

--- controllers/port_scan.py
+++ controllers/port_scan.py
@@ -1,21 +1,15 @@
 import os
 from flask_restful import Resource, reqparse, request, inputs
 import socket
-import redis
-from rq import Retry, Queue
-import json
 import pymongo
 from datetime import datetime, timedelta
 import validators
-from helpers import auth_check, queue_to_db, port_scan_rec
-from helpers.requests_retry import retry_session
-import logging
+from helpers import auth_check, port_scan_rec, port_scan_nmap
+from helpers.mongo_connection import db
+from helpers.queue_to_db import port_scan_db_addition
 
-client = pymongo.MongoClient(os.environ.get('MONGO_CONN'))
-db = client.test
 
 # add_to_db = Queue(name='portScan_db_queue', connection=redis.from_url(url=os.environ.get('REDIS_CONN_STRING')), default_timeout=-1)
-logging.info(f"Environment variable {os.environ.get('REDIS_CONN_STRING')} to Redis Conn String")
 portscan_args = reqparse.RequestParser()
 
 portscan_args.add_argument('value', help='Domain or IP is required to scan', required=True)
@@ -39,72 +33,44 @@ class PortScan(Resource):
 
         args = portscan_args.parse_args()
 
-        list_scans = {}
+        scan_out = {}
         val = args['value']
-        db.portScan.create_index('value')
+        db.portScan.create_index([('value', pymongo.DESCENDING), ('status', pymongo.DESCENDING)])
         # see if we have an existing scan for given value and pull the latest
-        search = db.portScan.find_one({'value': val}, sort=[('_id', pymongo.DESCENDING)])
+        search = db.portScan.find_one({'value': val, 'status': 'finished'}, sort=[('_id', pymongo.DESCENDING)])
 
         # force comes in as false by default
-        if args['force']:
+        # flipping functionality so ePlace doesn't need a change on their end
+        if not args['force']:
             force = True
         elif search is not None:
-            force = search['timeStamp'] + timedelta(days=2) < datetime.utcnow()
+            force = search['timeStamp'] + timedelta(days=3) < datetime.utcnow()
 
         if search is None or force is True:
 
             if validators.domain(val) or validators.ip_address.ipv4(val):
-                ip = socket.gethostbyname(val)
 
-                item = db.portScan.insert_one(
-                    {'ip': ip, 'value': val,
-                     "timeStamp": datetime.utcnow()}).inserted_id
+                try:
+                    ip = socket.gethostbyname(val)
+                except:
+                    return {
+                               'message': f"{val} does not exists or cannot be reached now, please check and try again"
+                           }, 400
 
-                list_scans['ip'] = ip
-                list_scans['value'] = val
+                scan_out['ip'] = ip
+                scan_out['value'] = val
 
-                # calling api with retries and backoff_factor
-                # session = retry_session()
-                # resp = session.get(f"https://api.viewdns.info/portscan/?host={val}&apikey="
-                #                    f"{os.environ.get('API_KEY_VIEW_DNS')}&output=json")
-                # logging.info(f"Environment variable {os.environ.get('API_KEY_VIEW_DNS')} to view DNS")
-                # if resp.status_code == 200:
-                #     out = json.loads(resp.content.decode())['response']
-                #
-                #     list_scans['portScan'] = out['port']
-                list_scans['portScan'] = None
+                out = port_scan_rec.port_scan(ip, 'regular')
 
-                out3 = {}
-
-                # Our internal port scan with multithreading
-                out1 = port_scan_rec.callback({'ip': ip,
-                                              'type': 'fast'})
-                logging.info(f"Output of out1, {out1}")
-
-                if len(out1) >= 3:
-                    out2 = port_scan_rec.callback({'ip': ip,
-                                                  'type': 'medium'})
-                    out1.update(out2)
-
-                    if len(out1) >= 4:
-                        out3 = port_scan_rec.callback({'ip': ip,
-                                                      'type': 'slow'})
-
-                        out1.update(out3)
-
-                list_scans['internalPortScan'] = out1
+                scan_out['count'] = len(out)
+                scan_out['internalPortScan'] = out
             else:
                 return {
                            'message': f'{val} is not a valid IP or Domain, please try again'
                        }, 400
         else:
-            del search['_id']
-            del search['timeStamp']
-            return search
+            return search['output']
 
-        message = {'mongo': str(item),
-                   'data': list_scans}
+        port_scan_db_addition(val, scan_out)
 
-        # add_to_db.enqueue(queue_to_db.port_scan_db_addition, message, retry=Retry(max=3, interval=[10, 30, 60]))
-
-        return list_scans
+        return scan_out
