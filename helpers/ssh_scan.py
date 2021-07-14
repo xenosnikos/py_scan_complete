5bfc36a1be833d5b52c59e4a2c066a66d5e2074c
Duncan Miller <duncanidaho488@gmail.com>
2021-07-13 22:12:51 -0500

    check protocol version, auth methods, known bad keys, and supported algorithms

--- helpers/ssh_scan.py
+++ helpers/ssh_scan.py
@@ -18,6 +18,17 @@ def process(ip):
     nmap_patch = nmap.PortScanner()
 
     try:
-        patch_check = nmap_patch.scan(hosts=ip, arguments='-p 22 -T4 -Pn -d')
+        patch_check = nmap_patch.scan(hosts=ip, arguments='-p 22 -sV -T4 -Pn -d '
+                                                          '--script ssh2-enum-algos '
+                                                          '--script ssh-publickey-acceptance '
+                                                          '--script ssh-auth-methods')
     except Exception as e:
         logger.error(f'SSH scan failed with exception {e}')
+    else:
+        #Handle results from nmap
+        protocol_version = patch_check['scan'][ip]['tcp'][22]['extrainfo']
+        auth_methods = patch_check['scan']['143.198.99.85']['tcp'][22]['script']['ssh-auth-methods']
+        known_bad_keys = patch_check['scan']['143.198.99.85']['tcp'][22]['script']['ssh-publickey-acceptance']
+        ssh_algos = patch_check['scan']['143.198.99.85']['tcp'][22]['script']['ssh2-enum-algos']
+
+    logger.debug(f"SSH scan complete for {ip}")
